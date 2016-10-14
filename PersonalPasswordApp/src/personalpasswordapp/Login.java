06b5e22687eebdeb067253dcdec41b50684252c2
Jack Bosley <Jack Bosley@104.39.61.45>
2016-10-14 12:01:02 -0400

    I hope this works.

--- PersonalPasswordApp/src/personalpasswordapp/Login.java
+++ PersonalPasswordApp/src/personalpasswordapp/Login.java
@@ -5,10 +5,16 @@
  */
 package personalpasswordapp;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 /**
  *
@@ -17,6 +23,27 @@ import java.nio.file.Paths;
 public class Login {
     String fileName = "temp.txt";
     String line = null;
+    private JLabel label, message;
+    private JTextField inputName;
+    private JPasswordField inputPass;
+    private JButton register;
+    
+    public Login()
+    {
+        super("Login");
+        setLayout(new FlowLayout(FlowLayout.LEADING));
+        label = new JLabel("Please enter Username and Password.");
+        add(label);
+        
+        message = new JLabel("Username: ");
+        add(message);
+        inputName = new JTextField(10);
+        add(inputName);
+        message = new JLabel("Password: ");
+        add(message);
+        inputPass = new JPasswordField(10);
+        add(inputPass);
+    }
     
      public static boolean checkLogin(String username, String password) throws IOException{
          boolean checked = false;
