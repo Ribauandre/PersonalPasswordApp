d9df50198b0b618680ce1ebc58190125184bb89b
andreribau <andreribau@client-130-203-89-185.mobility.psu.edu>
2016-10-14 09:12:00 -0400

    test

--- PersonalPasswordApp/src/personalpasswordapp/Registration.java
+++ PersonalPasswordApp/src/personalpasswordapp/Registration.java
@@ -107,15 +107,5 @@ public class Registration extends JFrame {
         return initPassword;
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Registration app = new Registration();
-        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        app.setSize(250, 150);
-        app.setVisible(true);
-        
-        // TODO code application logic here
-    }
+
 }
