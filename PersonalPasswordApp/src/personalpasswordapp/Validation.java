2e0852dc387161f02efe489889a939172a3b36f7
andreribau <andreribau@client-130-203-89-185.mobility.psu.edu>
2016-10-14 09:46:42 -0400

    test

---
+++ PersonalPasswordApp/src/personalpasswordapp/Validation.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalpasswordapp;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+public class Validation {
+    static boolean isPop = false;
+    
+    public static void checkPop() throws IOException{
+        
+        String fileName = "register.text";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "canno't find file file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+        if(line != null){
+            isPop = true;
+        }
+    }   
+    
+}
+
+
