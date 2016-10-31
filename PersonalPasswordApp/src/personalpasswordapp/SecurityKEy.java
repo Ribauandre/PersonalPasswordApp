71679637db5bb3f0755fddf9221265308d87ac21
rds5451 <rds5451@110-WL47.up.ist.local>
2016-10-31 09:37:37 -0400

    test

--- PersonalPasswordApp/src/personalpasswordapp/SecurityKEy.java
+++ PersonalPasswordApp/src/personalpasswordapp/SecurityKEy.java
@@ -5,10 +5,28 @@
  */
 package personalpasswordapp;
 
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JPasswordField;
+
 /**
  *
  * @author rds5451
  */
-public class SecurityKEy {
-    
+public class SecurityKEy 
+{
+    //add section here
 }
