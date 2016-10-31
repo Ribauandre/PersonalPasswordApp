bf6502499d543d3db6ebbb8ce8c095dfa4a00542
rds5451 <rds5451@110-WL47.up.ist.local>
2016-10-31 09:40:16 -0400

    test

---
+++ PersonalPasswordApp/src/personalpasswordapp/Password.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalpasswordapp;
+
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JPasswordField;
+
+
+/**
+ *
+ * @author rds5451
+ */
+public class Password 
+{
+    
+}
