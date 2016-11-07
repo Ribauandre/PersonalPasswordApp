1d19fc77f829dbb25397c78477d937e252fa2494
andreribau <andreribau@client-130-203-89-185.mobility.psu.edu>
2016-11-07 10:53:40 -0500

    test

---
+++ PersonalPasswordApp/src/personalpasswordapp/AccountsOB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personalpasswordapp;
+
+/**
+ *
+ * @author andreribau
+ */
+public class AccountsOB {
+    String accountName;
+    String accountUser;
+    String accountPass;
+    
+    public AccountsOB(String name, String user, String pass ){
+        accountName = name;
+        accountUser = user;
+        accountPass = pass;
+    }
+}
