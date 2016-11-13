4ddbc072a46d72a0b69ffc3b710aafc1808fc7d3
andreribau <andreribau@client-130-203-89-185.mobility.psu.edu>
2016-11-13 17:11:14 -0500

    andre

--- PersonalPasswordApp/src/personalpasswordapp/Password.java
+++ PersonalPasswordApp/src/personalpasswordapp/Password.java
@@ -19,15 +19,100 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.JPasswordField;
 
 
 /**
  *
- * @author rds5451
+ * @author andreribau
  */
-public class Password 
-{
+public class Password extends JFrame {
     
+    private JLabel label, message;
+    private JButton add, back, delete;
+    public AccountsOB account;
+    public String select;
+    public String accName;
+    public String accUser;
+    public String accPass;
+
+    
+public Password()
+    {
+        super("Account");
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        label = new JLabel("Account Info");
+        c.gridx = 0;
+        c.gridwidth = 3;
+        c.gridy = 0;
+        add(label, c);
+        
+        for(int i = 0; i<Validation.accounts.size(); i++){
+            if(Validation.accounts.get(i).accountName.equals(Account.select)){
+                accName = Validation.accounts.get(i).accountName;
+                accUser = Validation.accounts.get(i).accountUser;
+                accPass = Validation.accounts.get(i).accountPass;  
+            }
+        }
+        
+        label = new JLabel("Account Name: "+accName);
+        c.gridx = 1;
+        c.gridwidth = 0;
+        c.gridy = 1;
+        add(label, c);
+        
+        label = new JLabel("Account Username: "+accUser);
+        c.gridx = 1;
+        c.gridwidth = 0;
+        c.gridy = 2;
+        add(label, c);
+        
+        
+        label = new JLabel("Account Password: "+accPass);
+        c.gridx = 1;
+        c.gridwidth = 0;
+        c.gridy = 3;
+        add(label, c);
+        
+        back = new JButton("Return");
+        c.gridwidth = 1;
+        c.gridx = 1;
+        c.gridy = 5;
+        c.anchor = GridBagConstraints.EAST;
+        c.weightx = 1;
+        add(back, c);
+        back.addActionListener((ActionEvent event) -> {
+            Account account = new Account();
+                    account.setVisible(true);
+                    account.setSize(400, 150);
+                    account.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                    dispose();
+        });
+        
+        delete = new JButton("Delete");
+        c.gridwidth = 1;
+        c.gridx = 1;
+        c.gridy = 4;
+        c.anchor = GridBagConstraints.EAST;
+        c.weightx = 1;
+        add(delete, c);
+        delete.addActionListener((ActionEvent event) -> {
+            Delete.removeLineFromFile("accounts.text", accName);
+            Validation.accounts.clear();
+            Account account = new Account();
+            account.setVisible(true);
+            account.setSize(400, 150);
+            account.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            dispose();
+
+        });
+        
+        
+        
+    }
 }
