72311e958d178befd0952d7557f263853859efd1
Bryan Ingram <bryan.ingram715@gmail.com>
2016-11-16 11:15:16 -0500

    Updated Password

--- PersonalPasswordApp/src/personalpasswordapp/Password.java
+++ PersonalPasswordApp/src/personalpasswordapp/Password.java
@@ -6,23 +6,13 @@
 package personalpasswordapp;
 
 
-import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
-import javax.swing.JPasswordField;
 
 
 /**
@@ -31,8 +21,8 @@ import javax.swing.JPasswordField;
  */
 public class Password extends JFrame {
     
-    private JLabel label, message;
-    private JButton add, back, delete;
+    private JLabel label;
+    private JButton back, delete;
     public AccountsOB account;
     public String select;
     public String accName;
@@ -45,10 +35,11 @@ public Password()
         super("Account");
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+        GridBagConstraints c2 = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
-        label = new JLabel("Account Info");
+        label = new JLabel("<HTML><FONT color=\"#00009\"><b><h3>Account Info</b></h3></FONT></HTML>");
         c.gridx = 0;
-        c.gridwidth = 3;
+        c.gridwidth = 1;
         c.gridy = 0;
         add(label, c);
         
@@ -60,54 +51,64 @@ public Password()
             }
         }
         
-        label = new JLabel("Account Name: "+accName);
+        label = new JLabel("Account Name:   ");
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 1;
+        add(label, c);
+        label = new JLabel(accName);
         c.gridx = 1;
-        c.gridwidth = 0;
+        c.gridwidth = 1;
         c.gridy = 1;
         add(label, c);
         
-        label = new JLabel("Account Username: "+accUser);
+        label = new JLabel("Account Username:   ");
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 2;
+        add(label, c);
+        label = new JLabel(accUser);
         c.gridx = 1;
-        c.gridwidth = 0;
+        c.gridwidth = 1;
         c.gridy = 2;
         add(label, c);
         
-        
-        label = new JLabel("Account Password: "+accPass);
+        label = new JLabel("Account Password:   ");
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 3;
+        add(label, c);
+        label = new JLabel(accPass);
         c.gridx = 1;
-        c.gridwidth = 0;
+        c.gridwidth = 1;
         c.gridy = 3;
         add(label, c);
         
         back = new JButton("Return");
-        c.gridwidth = 1;
-        c.gridx = 1;
-        c.gridy = 5;
-        c.anchor = GridBagConstraints.EAST;
-        c.weightx = 1;
-        add(back, c);
+        c2.gridwidth = 1;
+        c2.gridx = 0;
+        c2.gridy = 4;
+        add(back, c2);
         back.addActionListener((ActionEvent event) -> {
-            Account account = new Account();
-                    account.setVisible(true);
-                    account.setSize(400, 150);
-                    account.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            Account acc = new Account();
+                    acc.setVisible(true);
+                    acc.setSize(400, 150);
+                    acc.setDefaultCloseOperation(EXIT_ON_CLOSE);
                     dispose();
         });
         
         delete = new JButton("Delete");
-        c.gridwidth = 1;
-        c.gridx = 1;
-        c.gridy = 4;
-        c.anchor = GridBagConstraints.EAST;
-        c.weightx = 1;
-        add(delete, c);
+        c2.gridwidth = 1;
+        c2.gridx = 1;
+        c2.gridy = 4;
+        add(delete, c2);
         delete.addActionListener((ActionEvent event) -> {
             Delete.removeLineFromFile("accounts.text", accName);
             Validation.accounts.clear();
-            Account account = new Account();
-            account.setVisible(true);
-            account.setSize(400, 150);
-            account.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            Account acc = new Account();
+            acc.setVisible(true);
+            acc.setSize(400, 150);
+            acc.setDefaultCloseOperation(EXIT_ON_CLOSE);
             dispose();
 
         });
